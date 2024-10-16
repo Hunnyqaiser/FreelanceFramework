146c8360d83bac67465f12928b57685c77621f2b
Muhammad Qaiser <muhammadqaiser64@yahoo.com>
2024-10-16 17:05:32 +0100

    added new testcase

--- src/main/java/test/Pages/LoginPage.java
+++ src/main/java/test/Pages/LoginPage.java
@@ -16,7 +16,7 @@ public class LoginPage
 	protected By pass_field = By.xpath("//input[@name='password1']");
 	protected By login_btn = By.xpath("//button[normalize-space()='Log in']");
 	protected By signIn_btn = By.xpath("//button[@class='submit-btn']");
-	
+	protected By signUp_btn = By.xpath("//a[normalize-space()='New user? Signup']");
 	
 	public HomePage loginToApp(String user_name, String password)
 	{
@@ -29,5 +29,12 @@ public class LoginPage
 		HomePage home=new HomePage(driver);
 		return home;
 		}
+	
+	public Sign_upPage clickOnSignUpButton()
+	{
+		Utility.waitForElement(driver, signUp_btn).click();
+		Sign_upPage signup=new Sign_upPage(driver);
+		return signup;
+	}
 
 }
