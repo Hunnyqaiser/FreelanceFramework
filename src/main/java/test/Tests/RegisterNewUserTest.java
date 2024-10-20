d0b1a9bb5970da9494ff0643cae3916d2fe04edb
qaiser-89 <149986935+qaiser-89@users.noreply.github.com>
2024-10-20 16:51:18 +0100

    Implemented the Random class for String Values, left 2 values for TDD in Excel

--- src/main/java/test/Tests/RegisterNewUserTest.java
+++ src/main/java/test/Tests/RegisterNewUserTest.java
@@ -3,6 +3,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.DataProvider.DataProvidersClass;
 import com.base.BaseClass;
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+
 import test.Pages.LoginPage;
 import test.Pages.Sign_upPage;
 
@@ -11,25 +14,14 @@ public class RegisterNewUserTest extends BaseClass
 	
 	
 	@Test(dataProvider="TestDataForAddingUser", dataProviderClass = DataProvidersClass.class)
-	public void registerNewUserWithDataProvider(String name, String email, String pass, String intrest, String gender, String state, String hobby)
+	public void registerNewUserWithDataProvider(String name,String state)
 	{
 		LoginPage login=new LoginPage(driver);
 		login.clickOnSignUpButton();
 		Sign_upPage signup=new Sign_upPage(driver);
-		signup.registerUser(name, email, pass, intrest, gender, state, hobby);
+		signup.registerUser(name, state);
 		boolean status=signup.signUpMess();
 		Assert.assertTrue(status, "Registration failed");
-	}
-	
-	@Test()
-	public void registerUserWithFaker()
-	{
-		
-		LoginPage login=new LoginPage(driver);
-		login.clickOnSignUpButton();
-		Sign_upPage signup=new Sign_upPage(driver);
-		signup.registerUser(null, null, null, null, null, null, null);
-		
 	}
 
 }
