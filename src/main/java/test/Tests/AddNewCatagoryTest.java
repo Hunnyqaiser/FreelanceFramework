1caf0554821d9fbf473a2266ca65027ccf287d12
qaiser-89 <149986935+qaiser-89@users.noreply.github.com>
2024-10-29 22:04:45 +0000

    added testcase to add catagory but alert is not working as expected

---
+++ src/main/java/test/Tests/AddNewCatagoryTest.java
@@ -0,0 +1,41 @@
+package test.Tests;
+
+import org.testng.annotations.Test;
+import com.DataProvider.DataProvidersClass;
+import com.Helper.Utility;
+import com.base.BaseClass;
+import test.Pages.HomePage;
+import test.Pages.LoginPage;
+import test.Pages.ManageCatagoriesPage;
+
+public class AddNewCatagoryTest extends BaseClass
+{
+	@Test(dataProvider = "TestData", dataProviderClass = DataProvidersClass.class, priority = 1)
+	public void navigateToPage(String name, String pass)
+	{
+		LoginPage login=new LoginPage(driver);
+		HomePage HP=new HomePage(driver);
+		ManageCatagoriesPage MCP=new ManageCatagoriesPage(driver);
+		login.loginToApp(name, pass);
+		HP.travelToManageCatagoriesPage();
+		MCP.verifyManageCatagoriesIsDisplayed();
+	}
+	
+	@Test(priority = 2)
+	public void getCatagoriesDetails()
+	{
+		ManageCatagoriesPage MCP=new ManageCatagoriesPage(driver);
+		MCP.FindNameAndNumberOfCatagories();
+	}
+	
+	@Test(priority = 3)
+	public void addNewCatagory()
+	{
+		ManageCatagoriesPage MCP=new ManageCatagoriesPage(driver);
+		MCP.addNewCatagory("Automation Testing");
+		System.out.println("Names and number of Catagories after adding my own!!!");
+		MCP.FindNameAndNumberOfCatagories();
+	}
+	
+
+}
